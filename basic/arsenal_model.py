d6670ea47a066c108ee8af1becb1f079367acf4f
pzh <pzhwxy@gmail.com>
2025-12-01 17:04:47 +0800

    feat:decoder layer

--- basic/arsenal_model.py
+++ basic/arsenal_model.py
@@ -1,18 +1,23 @@
 import math
+from typing import Optional
 
 import torch
 from torch import nn
+from transformers import AutoTokenizer
 
 from config.arsenal_model_config import ArsenalConfig
+
+tokenizer = AutoTokenizer.from_pretrained('../tokenization', trust_remote_code=True, pad_token='<|endoftext|>')
 
 
 class ArsenalAttention(nn.Module):
     """多头注意力实现来自论文'Attention Is All You Need'
     论文链接:https://arxiv.org/abs/1706.03762"""
 
-    def __init__(self, config: ArsenalConfig):
+    def __init__(self, config: ArsenalConfig, layer_idx):
         super(ArsenalAttention, self).__init__()
         self.config = config
+        self.layer_idx = layer_idx
         self.head_dim = getattr(config, 'head_dim', config.hidden_size // config.num_attention_heads)
         if config.hidden_size % config.num_attention_heads != 0:
             raise ValueError("注意力头必须是隐藏层大小的整数倍")
@@ -86,3 +91,185 @@ class ArsenalAttention(nn.Module):
         # (bsz,seq_len,hidden_size)->(bsz,seq_len,hidden_size)
         attn_outputs = self.o_proj(attn_outputs)
         return attn_outputs, attn_weights
+
+
+class ArsenalLayerNorm(nn.Module):
+    """使用层归一化
+        公式为:(样本-均值/(方差+很小的数(默认1e-5)防止除零)的平方根)*缩放+偏移
+    """
+
+    def __init__(self, hidden_size, eps=1e-5):
+        super(ArsenalLayerNorm, self).__init__()
+        self.eps = eps
+        # 缩放和偏移
+        # scale和shift是可学习的参数让模型能够适应不同的数据分布
+        # 缩放被初始化为1,由公式可知应该被初始化为1
+        self.scale = nn.Parameter(torch.ones(hidden_size))
+        # 偏移被初始化为0,由公式可知应该被初始化为0
+        self.shift = nn.Parameter(torch.zeros(hidden_size))
+
+    def forward(self, hidden_states: torch.Tensor):
+        # 计算均值
+        mean = hidden_states.mean(dim=-1, keepdim=True)
+        # 计算方差
+        variance = hidden_states.var(dim=-1, keepdim=True, unbiased=False)
+        # 套入层归一化公式
+        hidden_states_normalized = (hidden_states - mean) / math.sqrt(variance + self.eps)
+        # 返回结果
+        return hidden_states_normalized * self.scale + self.shift
+
+
+class FeedForward(nn.Module):
+    """transformer架构中前馈神经网络处理"""
+
+    def __init__(self, config: ArsenalConfig):
+        super(FeedForward, self).__init__()
+        self.hidden_size = config.hidden_size
+        self.layers = nn.Sequential(
+            nn.Linear(self.hidden_size, config.intermediate_size),
+            # 使用GELU激活函数
+            nn.GELU(),
+            nn.Linear(config.intermediate_size, self.hidden_size),
+        )
+
+    def forward(self, hidden_states: torch.Tensor):
+        return self.layers(hidden_states)
+
+
+class ArsenalDecoderLayer(nn.Module):
+    """
+    transformer架构中Decoder层的实现
+    """
+
+    def __init__(self, config: ArsenalConfig, layer_idx):
+        super(ArsenalDecoderLayer, self).__init__()
+        self.hidden_size = config.hidden_size
+        # 自注意力层
+        self.self_attention = ArsenalAttention(config, layer_idx)
+        # 前馈神经网络处理
+        self.feed_forward = FeedForward(config)
+        # 归一化层
+        self.input_layer_norm = ArsenalLayerNorm(self.hidden_size, config.norm_eps)
+        self.post_attention_layer_norm = ArsenalLayerNorm(self.hidden_size, config.norm_eps)
+
+    def forward(self, hidden_states: torch.Tensor):
+        # 定义残差
+        residual = hidden_states
+        # 层归一化处理
+        hidden_states = self.input_layer_norm(hidden_states)
+        # 自注意力机制
+        hidden_states, _ = self.self_attention(hidden_states)
+        # 残差连接
+        hidden_states = residual + hidden_states
+        # 全连接层
+        residual = hidden_states
+        # 层归一化处理
+        hidden_states = self.post_attention_layer_norm(hidden_states)
+        # 前馈神经网络
+        hidden_states = self.feed_forward(hidden_states)
+        # 残差连接
+        hidden_states = residual + hidden_states
+        return hidden_states
+
+
+class ArsenalModel(nn.Module):
+    """
+    Arsenal大语言模型
+    """
+
+    def __init__(self, config: ArsenalConfig):
+        super(ArsenalModel, self).__init__()
+        self.pad_token_id = config.pad_token_id
+        self.eos_token_id = config.eos_token_id
+        self.context_length = config.context_length
+        self.num_layers = config.num_layers
+        self.vocab_size = config.vocab_size
+        self.embed_tokens = nn.Embedding(self.vocab_size, config.hidden_size, padding_idx=self.pad_token_id)
+        self.position_embeddings = nn.Embedding(config.max_position_embedding, config.hidden_size)
+        self.layers = nn.ModuleList(
+            [
+                ArsenalDecoderLayer(config, layer_idx) for layer_idx in range(self.num_layers)
+            ]
+        )
+        self.norm = ArsenalLayerNorm(config.hidden_size, config.norm_eps)
+        self.output_head = nn.Linear(config.hidden_size, config.vocab_size)
+
+    def forward(self, input_ids: Optional[torch.Tensor] = None):
+        if input_ids is None:
+            raise ValueError("input_ids不能为空")
+        # 词嵌入
+        input_embeds = self.embed_tokens(input_ids)
+        # 采用绝对位置方式计算位置编码
+        position_embeds = self.position_embeddings(input_ids)
+        # 词嵌入+位置编码
+        hidden_states = input_embeds + position_embeds
+        # 循环层处理
+        for decoder_layer in self.layers[:self.num_layers]:
+            hidden_states = decoder_layer(hidden_states)
+        # 归一化处理
+        hidden_states = self.norm(hidden_states)
+        # 输出结果(bsz,seq_len,hidden_size)->(bsz,seq_len,vocab_size)
+        return self.output_head(hidden_states)
+
+
+def generate(model: ArsenalModel, idx, temperature=0.0, top_k=None):
+    """
+    生成下一个词模块
+    """
+    for _ in range(model.context_length):
+        # 每个维度只取最后context_size元素 (bsz,seq_len)
+        idx_cond = idx[:, -model.context_length:]
+        # 获取结果
+        with torch.no_grad():
+            logits: torch.Tensor = model(idx_cond)
+        # 计算预测值,但是切最后一个 (bsz,seq_len,vocab_size)->(bsz,vocab_size)
+        logits = logits[:, -1, :]
+        # top K采样
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            top_k_value = top_logits[:, -1]
+            logits = torch.where(logits < top_k_value, torch.tensor(float("-inf")).to(logits.device), logits)
+
+        # 温度校正
+        if temperature > 0.0:
+            logits = logits / temperature
+            # 运用softmax归一化
+            probs = torch.softmax(logits, dim=-1)
+            # 调用随机采样 (bsz,vocab_size)->(batch_size, 1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            # 如果未启用采样，选择概率最高的token(bsz,vocab_size)->(batch_size, 1)
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        # 如果出现了结尾字符则停止生成
+        if idx_next == model.eos_token_id:
+            break
+        # 拼接idx (batch_size, seq_len)->(batch_size, seq_len+1)
+        idx = torch.cat((idx, idx_next), dim=1)  #
+    return idx
+
+
+def text_to_token_ids(text):
+    encoded = tokenizer.encode(text)
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)  # add batch dimension
+    return encoded_tensor
+
+
+# 给输入的字符进行编码并实现一个Batch维度的向量,符合模型的输入形式
+def token_ids_to_text(token_ids):
+    flat = token_ids.squeeze(0)  # remove batch dimension
+    return tokenizer.decode(flat.tolist())
+
+
+if __name__ == '__main__':
+    torch.manual_seed(123)
+    modelConfig = ArsenalConfig()
+    torch.device("cuda")
+    llm_model = ArsenalModel(modelConfig)
+    llm_model.to("cuda")
+    output_ids = generate(
+        model=llm_model,
+        idx=text_to_token_ids("hello"),
+        top_k=25,
+        temperature=1.4
+    )
+    print("Output text:\n", token_ids_to_text(output_ids))
--- config/arsenal_model_config.py
+++ config/arsenal_model_config.py
@@ -1,6 +1,8 @@
 class ArsenalConfig:
-    def __init__(self, vocab_size=151936, hidden_size=4096, num_attention_heads=23, num_layers=32, attention_bias=True,
-                 attention_dropout=0.0, head_dim=128, max_position_embedding=32768):
+    def __init__(self, vocab_size=151936, hidden_size=4096, num_attention_heads=16, num_layers=32, attention_bias=True,
+                 attention_dropout=0.0, head_dim=128, max_position_embedding=32768, norm_eps=1e-5,
+                 intermediate_size=12288, pad_token_id=151643, bos_token_id=151643, eos_token_id=151645,
+                 context_length=4096):
         r"""
         以下是Arsenal模型的基础配置参数
         :param vocab_size:  Arsenal模型的词表大小,默认为151936
@@ -11,6 +13,12 @@ class ArsenalConfig:
         :param attention_dropout: 训练时的丢弃率,默认值为0.0
         :param head_dim: 每个注意力头维度
         :param max_position_embedding: 能够处理的最大位置编码
+        :param norm_eps: 层归一化时参数,默认值为1e-5
+        :param intermediate_size: 输出时中间层大小
+        :param pad_token_id: 填充的词索引位置
+        :param bos_token_id: 开始的词索引位置
+        :param eos_token_id: 结束的词索引位置
+        :param context_length: 上下文长度
         """
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
@@ -19,4 +27,10 @@ class ArsenalConfig:
         self.attention_bias = attention_bias
         self.attention_dropout = attention_dropout
         self.head_dim = head_dim
+        self.norm_eps = norm_eps
         self.max_position_embedding = max_position_embedding
+        self.intermediate_size = intermediate_size
+        self.pad_token_id = pad_token_id
+        self.bos_token_id = bos_token_id
+        self.eos_token_id = eos_token_id
+        self.context_length = context_length
